f310a0d271ec1767a5facb0870ba5c7bb985a59a
Judicaël DUBRAY <djmx972@gmail.com>
2022-11-06 17:04:02 +0100

    finishe game

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,103 +1,39 @@
-use std::io;
-
-use macroquad::prelude::*;
+use macroquad::prelude::{coroutines::start_coroutine, *};
 use shared::{ClientMessage, RemoteState, ServerMessage, State};
-use tungstenite::Error;
+use std::{io, sync::Arc};
 use ws::Connection;
+
+mod tcpstream;
 mod ws;
 
 const PLANE_WIDTH: f32 = 32.;
 const PLANE_HEIGHT: f32 = 32.;
-#[macroquad::main("game")]
-async fn main() {
-    pretty_env_logger::init();
-    let mut connection = Connection::new();
-    connection.connect("ws://localhost:3030/game");
-    let mut game = Game::new().await;
-    loop {
-        let state = ClientMessage::State(State {
-            pos: game.player_state.position,
-            r: game.player_state.rotation,
-        });
-
-        client_send(&state, &mut connection);
-        if let Some(msg) = connection.poll() {
-            let msg = serde_json::from_slice::<ServerMessage>(msg.as_slice())
-                .expect("deserialization failed");
-
-            game.handle_message(msg);
-        }
-
-        game.update();
-        game.draw();
-        if game.quit {
-            return;
-        }
-        next_frame().await
-    }
-}
-pub fn client_send(msg: &ClientMessage, connection: &mut Connection) {
-    let bytes = serde_json::to_vec(msg).expect("serialization failed");
-    if let Err(err) = connection.send(bytes) {
-        log::error!("Failed to send msg: {}", err);
-
-        // if let Error::Io(err) = err {
-        //     if let io::ErrorKind::ConnectionReset | io::ErrorKind::ConnectionAborted = err.kind() {
-        //         log::error!("Connection lost, attempting to reconnect");
-        //         connection.connect("ws://localhost:3030/game");
-        //     }
-        // }
-    }
-}
-
-pub struct PlayerState {
-    pub id: usize,
-    pub position: Vec2,
-    pub rotation: f32,
-}
 
 pub struct Game {
     pub quit: bool,
-    pub texture: Texture2D,
     pub player_state: RemoteState,
+    pub texture: Texture2D,
     pub remote_states: Vec<RemoteState>,
 }
-fn draw_box(pos: Vec2, size: Vec2) {
-    let dimension = size * 2.;
-    let upper_left = pos - size;
 
-    draw_rectangle(upper_left.x, upper_left.y, dimension.x, dimension.y, BLACK)
-}
-
-pub fn vec2_from_angle(angle: f32) -> (f32, f32) {
-    let angle = angle - std::f32::consts::FRAC_PI_2;
-    (angle.cos(), angle.sin())
-}
 impl Game {
     pub async fn new() -> Self {
-        let texture = load_texture("assets/planes.png")
-            .await
-            .expect("Failed to load the plane texture!");
+        let texture = load_texture("assets/planes.png").await.unwrap();
+
         Self {
-            quit: false,
-            remote_states: Vec::new(),
             player_state: RemoteState::new(0, 100f32, 100f32, 0f32),
             texture,
+            quit: false,
+            remote_states: Vec::new(),
         }
     }
 
     pub fn update(&mut self) {
-        const ROT_SPEED: f32 = 0.015;
-
-        const SPEED: f32 = 0.6;
-
-        let (x, y) = vec2_from_angle(self.player_state.rotation);
-
-        self.player_state.update_position(x, y, SPEED);
-
         if is_key_down(KeyCode::Escape) {
             self.quit = true;
         }
+        const ROT_SPEED: f32 = 0.015;
+
         if is_key_down(KeyCode::Right) {
             self.player_state.rotation += ROT_SPEED;
         }
@@ -105,11 +41,21 @@ impl Game {
             self.player_state.rotation -= ROT_SPEED;
         }
 
+        const SPEED: f32 = 0.6;
+        let (x, y) = vec2_from_angle(self.player_state.rotation);
+        self.player_state.update_position(x, y, SPEED);
+
+        for state in &mut self.remote_states {
+            let (x, y) = vec2_from_angle(state.rotation);
+            state.update_position(x, y, SPEED);
+        }
+
         if self.player_state.position.x > screen_width() {
             self.player_state.position.x = -PLANE_WIDTH;
         } else if self.player_state.position.x < -PLANE_WIDTH {
             self.player_state.position.x = screen_width();
         }
+
         if self.player_state.position.y > screen_height() {
             self.player_state.position.y = -PLANE_HEIGHT;
         } else if self.player_state.position.y < -PLANE_HEIGHT {
@@ -138,13 +84,13 @@ impl Game {
                 rotation: state.rotation,
                 ..Default::default()
             },
-        )
+        );
     }
 
     pub fn draw(&self) {
         clear_background(color_u8!(255, 255, 255, 255));
 
-        draw_box(Vec2::new(200f32, 200f32), Vec2::new(10f32, 10f32));
+        draw_box(Vec2::new(400f32, 200f32), Vec2::new(50f32, 20f32));
 
         self.draw_plane(&self.player_state);
 
@@ -165,5 +111,81 @@ impl Game {
                 self.remote_states = remote_states;
             }
         }
+    }
+}
+
+pub fn vec2_from_angle(angle: f32) -> (f32, f32) {
+    let angle = angle - std::f32::consts::FRAC_PI_2;
+    (angle.cos(), angle.sin())
+}
+
+fn draw_box(pos: Vec2, size: Vec2) {
+    let dimension = size * 2.;
+    let upper_left = pos - size;
+
+    draw_rectangle(upper_left.x, upper_left.y, dimension.x, dimension.y, BLACK);
+}
+
+pub async fn client_connect(connection: Arc<Connection>, url: &str) {
+    if let Err(err) = connection.connect(url).await {
+        log::error!("Failed to connect to {}: {}", url, err);
+    }
+}
+
+pub fn client_send(msg: &ClientMessage, connection: Arc<Connection>) {
+    let bytes = serde_json::to_vec(&msg).expect("serialization failed");
+    if let Err(err) = connection.send(bytes) {
+        log::error!("Failed to send: {}", err);
+
+        if let tungstenite::Error::Io(err) = err {
+            if let io::ErrorKind::ConnectionReset | io::ErrorKind::ConnectionAborted = err.kind() {
+                log::error!("Connection lost, attempting to reconnect");
+                connection.restart();
+                start_coroutine(client_connect(
+                    connection.clone(),
+                    "ws://localhost:3030/game",
+                ));
+            }
+        }
+    }
+}
+
+pub fn client_receive(game: &mut Game, connection: Arc<Connection>) {
+    if let Some(msg) = connection.poll() {
+        let msg: ServerMessage =
+            serde_json::from_slice(msg.as_slice()).expect("deserialization failed");
+        game.handle_message(msg);
+    }
+}
+
+#[macroquad::main("game")]
+async fn main() {
+    pretty_env_logger::init();
+
+    let mut game = Game::new().await;
+
+    let connection = Arc::new(Connection::new());
+    let connection_coroutine = start_coroutine(client_connect(
+        connection.clone(),
+        "ws://localhost:3030/game",
+    ));
+
+    loop {
+        if connection_coroutine.is_done() {
+            let state = ClientMessage::State(State {
+                pos: game.player_state.position,
+                r: game.player_state.rotation,
+            });
+            client_send(&state, connection.clone());
+            client_receive(&mut game, connection.clone());
+
+            game.update();
+            game.draw();
+        }
+
+        if game.quit {
+            return;
+        }
+        next_frame().await
     }
 }
